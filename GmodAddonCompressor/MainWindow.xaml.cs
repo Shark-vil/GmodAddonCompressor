73cef6533c7a2c16515bf7574b960b385fb85653
Shark-vil <shark_vil@mail.ru>
2022-09-25 04:22:55 +0300

    Update version: 2.0.1

--- GmodAddonCompressor/MainWindow.xaml.cs
+++ GmodAddonCompressor/MainWindow.xaml.cs
@@ -10,7 +10,7 @@ namespace GmodAddonCompressor
     public partial class MainWindow : Window
     {
         private MainWindowContext _context = new MainWindowContext();
-        private const string _version = "v2.0.0";
+        private const string _version = "v2.0.1";
 
         public MainWindow()
         {
