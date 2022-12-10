3f8a6f0e5591ee25bdaa58baa450fc585a3a0896
Shark-vil <shark_vil@mail.ru>
2022-12-10 16:11:19 +0300

    Not convert wav, if exists loop points

--- GmodAddonCompressor/Objects/WAVEdit.cs
+++ GmodAddonCompressor/Objects/WAVEdit.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using NAudio.Wave;
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace GmodAddonCompressor.Objects
@@ -20,6 +21,51 @@ namespace GmodAddonCompressor.Objects
 
             if (File.Exists(newWavFilePath))
                 File.Delete(newWavFilePath);
+
+            try
+            {
+                bool wavIsLooping = false;
+
+                using (var fs = new FileStream(wavFilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    while (fs.Position < fs.Length)
+                    {
+                        long chunkStartPos = fs.Position;
+                        string chunkId = ReadBytesToString(fs, 4);
+                        uint chunkSize = ReadBytesToInt(fs, 4);
+                        long chunkEndPos = chunkStartPos + chunkSize + 8;
+
+                        switch (chunkId.ToUpper().Trim())
+                        {
+                            case "RIFF":
+                                ReadBytes(fs, 4);
+                                break;
+                            case "SMPL":
+                                wavIsLooping = true;
+                                break;
+                            case "CUE":
+                                wavIsLooping = true;
+                                break;
+                            default:
+                                fs.Position = chunkEndPos;
+                                break;
+                        }
+
+                        if (wavIsLooping)
+                            break;
+                    }
+                }
+
+                if (wavIsLooping)
+                {
+                    _logger.LogWarning($"Converting of looped files is not supported now! ( {wavFilePath} )");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+            }
 
             using (var reader = new WaveFileReader(wavFilePath))
             {
@@ -89,6 +135,27 @@ namespace GmodAddonCompressor.Objects
 
             if (File.Exists(newWavFilePath))
                 File.Delete(newWavFilePath);
+        }
+
+        private uint ReadBytesToInt(FileStream fs, int byteNum)
+        {
+            return BitConverter.ToUInt32(ReadBytes(fs, byteNum), 0);
+        }
+
+        private string ReadBytesToString(FileStream fs, int byteNum)
+        {
+            return System.Text.Encoding.UTF8.GetString(ReadBytes(fs, byteNum)).Trim('\0');
+        }
+
+        private byte[] ReadBytes(FileStream fs, int num)
+        {
+            byte[] bytes = new byte[num < 4 ? 4 : num];
+            for (int i = 0; i < num; i++)
+            {
+                int b = fs.ReadByte();
+                bytes[i] = (byte)b;
+            }
+            return bytes;
         }
     }
 }
